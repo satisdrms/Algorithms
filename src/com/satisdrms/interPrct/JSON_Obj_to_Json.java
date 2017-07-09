f72c4130fcda2daf27ac05c6eebcd5c7247a8351
sathish <sathish@192.168.153.1>
2017-07-09 20:52:53 +0530

    comments

--- src/com/satisdrms/interPrct/JSON_Obj_to_Json.java
+++ src/com/satisdrms/interPrct/JSON_Obj_to_Json.java
@@ -3,21 +3,22 @@ package com.satisdrms.interPrct;
 import java.io.File;
 import java.io.IOException;
 
-import org.codehaus.jackson.JsonGenerationException;
+//import org.codehaus.jackson.JsonGenerationException;
 import org.codehaus.jackson.map.JsonMappingException;
 import org.codehaus.jackson.map.ObjectMapper;
 
 public class JSON_Obj_to_Json {
 
-	public static void main(String[] args) throws JsonGenerationException,
-			JsonMappingException, IOException {
+	public static void main(String[] args) 
+			//throws JsonGenerationException,JsonMappingException, IOException 
+	{
 		
 		ObjectMapper mapper = new ObjectMapper();
 		String jsonInString = "{'name' : 'mkyong'}";
 
 		Staff obj = new Staff();
 		
-		mapper.writeValue(new File("staff.json"), obj);
+		//mapper.writeValue(new File("staff.json"), obj);
 
 	}
 
