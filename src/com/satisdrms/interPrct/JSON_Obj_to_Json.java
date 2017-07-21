b2943bd223f130329b23f66f5b12aafa5202c3c2
SATIS <ssathishdreams@gmail.com>
2017-07-21 22:51:40 +0530

    error clearing

--- src/com/satisdrms/interPrct/JSON_Obj_to_Json.java
+++ src/com/satisdrms/interPrct/JSON_Obj_to_Json.java
@@ -4,8 +4,8 @@ import java.io.File;
 import java.io.IOException;
 
 //import org.codehaus.jackson.JsonGenerationException;
-import org.codehaus.jackson.map.JsonMappingException;
-import org.codehaus.jackson.map.ObjectMapper;
+//import org.codehaus.jackson.map.JsonMappingException;
+//import org.codehaus.jackson.map.ObjectMapper;
 
 public class JSON_Obj_to_Json {
 
@@ -13,7 +13,7 @@ public class JSON_Obj_to_Json {
 			//throws JsonGenerationException,JsonMappingException, IOException 
 	{
 		
-		ObjectMapper mapper = new ObjectMapper();
+	//	ObjectMapper mapper = new ObjectMapper();
 		String jsonInString = "{'name' : 'mkyong'}";
 
 		Staff obj = new Staff();
